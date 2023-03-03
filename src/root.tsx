56f30ddf18b20ac86e3e6630ac033d464de47b04
emilbui <frostwyrm9x@gmail.com>
2023-03-03 10:06:06 +0700

    Update link

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,6 @@
 // @refresh reload
 import {Suspense} from "solid-js";
 import {
-    A,
     Body,
     ErrorBoundary,
     FileRoutes,
@@ -36,13 +35,13 @@ export default function Root() {
                     <div class="footer-content">
                         <ul class="info">
                             <li><a
-                                href="https://github.com/Pustur/genshin-impact-team-randomizer"
+                                href="https://github.com/emilebui/GBP_Deploy"
                                 target="_blank"
                             >
                                 GitHub
                             </a></li>
                             <li><a
-                                href="#"
+                                href="https://medium.com/@dragonblade9x/making-a-multiplayer-web-game-with-web-socket-that-can-be-scalable-to-millions-of-users-923cc8bd4d3b"
                                 target="_blank"
                             >
                                 Development Blog
