19b6a505997f27a7bdee69467140f59833cbd2da
emilbui <frostwyrm9x@gmail.com>
2023-03-08 16:07:13 +0700

    update window scale fix

--- src/routes/spectate/[gameid].tsx
+++ src/routes/spectate/[gameid].tsx
@@ -1,13 +1,17 @@
 import {useParams} from "@solidjs/router";
 import Menu from "~/components/Menu/Menu";
+import Footer from "~/components/Footer/Footer";
 export default function Spectate() {
 
     const params = useParams();
     const gid = params.gameid
 
     return (
-          <Menu player="Anonymous" gid={gid} watch={true}>
-              <h1>Enter your nickname and watch the game !!!</h1>
-          </Menu>
+        <main>
+            <Menu player="Anonymous" gid={gid} watch={true}>
+                <h1>Enter your nickname and watch the game !!!</h1>
+            </Menu>
+            <Footer Absolute={true}></Footer>
+        </main>
     );
 }
