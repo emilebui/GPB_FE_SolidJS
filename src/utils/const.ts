fd9a8146b52b7da0158884df4b6835fbd1913169
EmilBui <bui.duy.anh@outpost24.com>
2024-06-05 17:27:41 +0700

    Update UI

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -8,8 +8,12 @@ const elementNames = [
   'pyro',
 ];
 
+const weaponNames = ['bow', 'catalyst', 'claymore', 'polearm', 'sword'];
+
+const genders = ['M', 'F'];
+
 const MaxTimer = 599 // 599 * 0.05s ~ 30s
 
 const rarities = [4, 5];
 
-export { elementNames, rarities, MaxTimer };
+export { elementNames, rarities, MaxTimer, weaponNames, genders };
